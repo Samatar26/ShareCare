0d4dd807bc6d1a2a071e5648ee967fb55f5e8d40
Samatar Axmed <Samatar_267@hotmail.com>
2017-06-17 14:04:59 +0100

    Remove index.css and create reset stylesheet in styled

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
-import './index.css';
 
 import reducers from './reducers';
 
---
+++ src/styles/reset.js
@@ -0,0 +1,20 @@
+import { injectGlobal } from 'styled-components';
+
+// eslint-disable-next-line
+injectGlobal`
+  html {
+    box-sizing: border-box;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+    margin: 0;
+    padding: 0;
+  }
+  body {
+   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
+   background-color: #222222;
+  }
+  ul {
+    list-style-type: none;
+  }
+`;
