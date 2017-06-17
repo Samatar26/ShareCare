1d6dcbff2310592a800e9398f461f55466f41fcd
Samatar Axmed <Samatar_267@hotmail.com>
2017-06-17 12:39:35 +0100

    Setup Create-React-App Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Begin
+      </div>
+    );
+  }
+}
+
+export default App;
