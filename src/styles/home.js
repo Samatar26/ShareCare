c29e698caa037a63e7fe6c8802629f241872fc31
Samatar Axmed <Samatar_267@hotmail.com>
2017-06-17 14:02:42 +0100

    Created homePage Styled Components

--- src/styles/home.js
+++ src/styles/home.js
@@ -1,18 +1,38 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const HomeHeader = styled.header`
+export const Header = styled.header`
 
 `;
 
-export const HomeSection = styled.section`
+export const HeaderBrand = styled.h1`
 
 `;
 
-export const HomeArticle = styled.article`
+export const HeaderOption = styled(Link)`
 
 `;
 
-export const HomeVideo = styled(Link)`
+export const Introduction = styled.p`
+
+`;
+
+export const UserOptions = styled.section`
+
+`;
+
+export const UserOption = styled.article`
+
+`;
+
+export const UserTitle = styled.h2`
+
+`;
+
+export const UserDescription = styled.p`
+  
+`;
+
+export const Video = styled(Link)`
 
 `;
--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
 import {
-  HomeHeader,
-  HomeSection,
-  HomeArticle,
-  HomeVideo,
+  Header,
+  HeaderBrand,
+  HeaderOption,
+  UserOptions,
+  UserOption,
+  UserTitle,
+  UserDescription,
+  Introduction,
+  Video,
 } from './../styles/home.js';
 
 class HomePage extends Component {
   render() {
     return (
       <div>
-        <HomeHeader />
-        <HomeSection>
-          <HomeArticle />
-          <HomeArticle />
-        </HomeSection>
-        <HomeVideo />
+        <Header>
+          <HeaderBrand>ShareCare</HeaderBrand>
+          <HeaderOption to="/signin">Sign in</HeaderOption>
+          <HeaderOption to="/singup">Sign up</HeaderOption>
+        </Header>
+        <Introduction>
+          CareBnB is a programme that matches people who want help or
+          support around the house with people who need accomodation and are
+          willing to help.
+        </Introduction>
+        <UserOptions>
+          <UserOption>
+            <UserTitle>Home Owner</UserTitle>
+            <UserDescription>
+              Are you a person who need some help at home and has a spare room
+              to offer?
+            </UserDescription>
+          </UserOption>
+          <UserOption>
+            <UserTitle>Home Seeker</UserTitle>
+            <UserDescription>
+              Are you a person looking for affordable accommodation and willing
+              to
+              help at least 10 hours per week in return for that?
+            </UserDescription>
+          </UserOption>
+        </UserOptions>
+        <Video to="/video">Watch Video</Video>
       </div>
     );
   }
