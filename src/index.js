cba4a8968981db9c471256b9332dfb6932ccfa18
Samatar Axmed <Samatar_267@hotmail.com>
2017-06-18 17:58:47 +0100

    Retrieved key from localstorage on page refresh

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import App from './components/App';
+import localforage from 'localforage';
+import { AUTH_USER } from './actions/Types';
 
 import reducers from './reducers';
 import './styles/reset';
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localforage.getItem('token').then(key => {
+  if (key) store.dispatch({ type: AUTH_USER });
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+  );
+});
