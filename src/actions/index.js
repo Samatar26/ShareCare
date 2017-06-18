f1a17b27d3c3c72d0aae718f46a4ca18f524bb1b
Samatar Axmed <Samatar_267@hotmail.com>
2017-06-18 14:36:01 +0100

    Add different error scenarios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import { AUTH_USER } from './Types';
+import localforage from 'localforage';
+import { AUTH_USER, AUTH_ERROR } from './Types';
 const ROOT_URL = 'http://localhost:4000';
 
 export function signinUser({ email, password }, history) {
@@ -11,9 +12,33 @@ export function signinUser({ email, password }, history) {
     })
       .then(res => res.json())
       .then(data => {
-        dispatch({ type: AUTH_USER });
-        history.push('/feature');
+        if (data.error) {
+          switch (data.error) {
+            case 'database error':
+              return authError(
+                'Sorry, there was a problem connecting to the database!'
+              );
+            case 'bcrypt':
+              return authError(
+                'Sorry, there was a problem checking your password!'
+              );
+            case 'User not found':
+              return authError("Sorry, the user doesn't exist!");
+          }
+          return;
+        } else {
+          localforage.setItem('token', data.token);
+          dispatch({ type: AUTH_USER });
+          history.push('/feature');
+        }
       })
-      .catch(err => console.log(err));
+      .catch(err => dispatch(authError('Network Problem sorry!')));
+  };
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error,
   };
 }
